0eab74b28cb5c933169b4a538e7918ab30ab87aa
JasperK04 <j.kleine.4@student.rug.nl>
2026-04-22 19:42:15 +0200

    refactor MapCoder into the existing structure

---
+++ src/evaluations/exec_outcome.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2024 Md. Ashraful Islam — Licensed under the MIT License. See LICENSE.
+from enum import Enum
+
+
+class ExecOutcome(Enum):
+    PASSED = "PASSED"  # code executes and output matches expected output
+    WRONG_ANSWER = (
+        "WRONG_ANSWER"  # code executes and output does NOT matches expected output
+    )
+    TIME_LIMIT_EXCEEDED = "TIME_LIMIT_EXCEEDED"  # code executes and didn't exit in time, output is ignored in this case
+    RUNTIME_ERROR = "RUNTIME_ERROR"  # code failed to execute (crashed)
+    COMPILATION_ERROR = "COMPILATION_ERROR"  # code failed to compile
+    MEMORY_LIMIT_EXCEEDED = (
+        "MEMORY_LIMIT_EXCEEDED"  # code exceeded memory limit during execution
+    )
---
+++ src/models/Base.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2024 Md. Ashraful Islam — Licensed under the MIT License. See LICENSE.
+import logging
+import sys
+
+import traceback
+from abc import ABC, abstractmethod
+
+
+class BaseModel(ABC):
+    def __init__(self, **kwargs):
+        pass
+
+    @abstractmethod
+    # @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(5))
+    def prompt(self, processed_input):
+        pass
+
