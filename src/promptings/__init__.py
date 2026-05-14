e362f7d0dbc89ac95c3a0a41297fcaac73e866b0
JasperK04 <j.kleine.4@student.rug.nl>
2026-05-14 13:46:16 +0200

    fix cleanup mistake

--- src/promptings/__init__.py
+++ src/promptings/__init__.py
@@ -1,15 +1,9 @@
-from .Analogical import AnalogicalStrategy
-from .CoT import CoTStrategy
 from .Direct import DirectStrategy
 from .MapCoder import MapCoder
 from .PACEcoding import PACEcoding
-from .SelfPlanning import SelfPlanningStrategy
 
 __all__ = [
-    "AnalogicalStrategy",
-    "CoTStrategy",
     "DirectStrategy",
     "MapCoder",
     "PACEcoding",
-    "SelfPlanningStrategy",
 ]
