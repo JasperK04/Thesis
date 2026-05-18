96a560b2113d798c674e2557f7838ed73a36db1d
JasperK04 <j.kleine.4@student.rug.nl>
2026-05-18 16:24:06 +0200

    fix ablation imports

--- src/promptings/ablation/__init__.py
+++ src/promptings/ablation/__init__.py
@@ -1,9 +1,9 @@
-from PACEcodingD import PACEcoding as PACEcodingD
-from PACEcodingP import PACEcoding as PACEcodingP
-from PACEcodingPD import PACEcoding as PACEcodingPD
-from PACEcodingR import PACEcoding as PACEcodingR
-from PACEcodingRD import PACEcoding as PACEcodingRD
-from PACEcodingRP import PACEcoding as PACEcodingRP
+from .PACEcodingD import PACEcoding as PACEcodingD
+from .PACEcodingP import PACEcoding as PACEcodingP
+from .PACEcodingPD import PACEcoding as PACEcodingPD
+from .PACEcodingR import PACEcoding as PACEcodingR
+from .PACEcodingRD import PACEcoding as PACEcodingRD
+from .PACEcodingRP import PACEcoding as PACEcodingRP
 
 __all__ = [
     "PACEcodingD",
